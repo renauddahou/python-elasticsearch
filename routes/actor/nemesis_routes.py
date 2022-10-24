fd88138988f425101efd3254fd6305cdea4c620a
genraven1 <genraven1@gmail.com>
2022-10-24 15:21:49 -0500

    Update method names (#7)

--- routes/actor/nemesis_routes.py
+++ routes/actor/nemesis_routes.py
@@ -4,11 +4,11 @@ from app import app, es
 from routes.base_routes import get_all, handle_single_result
 
 
-@app.route('/nemesis/')
+@app.route('/actors/nemesis/')
 def get_all_nemesis():
     return get_all(es, NEMESIS)
 
 
-@app.route('/nemesis/<name>', methods=['POST', 'GET', 'PUT'])
+@app.route('/actors/nemesis/<name>', methods=['POST', 'GET', 'PUT'])
 def handle_nemesis(name):
     return handle_single_result(es, NEMESIS, request)
